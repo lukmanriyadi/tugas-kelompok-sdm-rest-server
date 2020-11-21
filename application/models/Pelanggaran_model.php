7cc8828e334b518775d0ca13fe5113d22c4aa930
Lukman Riyadi <lukmanriyadi0@gmail.com>
2020-11-21 09:06:57 +0700

    typo pelanggaran

--- application/models/Pelanggaran_model.php
+++ application/models/Pelanggaran_model.php
@@ -5,7 +5,7 @@ class Pelanggaran_model extends CI_Model
     public function getPelanggaran($id = null)
     {
         if ($id === null) {
-            return $this->db->get('pelanggran')->result_array();
+            return $this->db->get('pelanggaran')->result_array();
         } else {
             return $this->db->get_where('pelanggaran', ['id_pelanggaran' => $id])->result_array();
         }
