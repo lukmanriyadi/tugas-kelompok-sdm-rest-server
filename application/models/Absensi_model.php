7e4d1462e43a2088a3d8e8ebfe71ec6528e69400
Lukman Riyadi <lukmanriyadi0@gmail.com>
2020-11-24 18:23:31 +0700

    benerin bagian gerabsensi tadi lupa

--- application/models/Absensi_model.php
+++ application/models/Absensi_model.php
@@ -5,9 +5,42 @@ class Absensi_model extends CI_Model
     public function getAbsensi($id = null)
     {
         if ($id === null) {
-            return $this->db->get('absensi')->result_array();
+            $this->db->select(
+                'absensi.id_absen,
+                pegawai.nama_depan,
+                pegawai.nama_belakang,
+                divisi.nama_divisi,
+                jabatan.nama_jabatan,
+                concat(absensi.periode_bulan, "-", absensi.periode_tahun) as periode,
+                absensi.tanggal as tanggal_absen,
+                absensi.status,
+                absensi.keterangan'
+            );
+            $this->db->from('absensi');
+            $this->db->join('pegawai', 'absensi.id_pegawai = pegawai.id_pegawai');
+            $this->db->join('jabatan', 'pegawai.id_jabatan = jabatan.id_jabatan');
+            $this->db->join('divisi', 'pegawai.id_divisi = divisi.id_divisi');
+            $query = $this->db->get();
+            return $query->result_array();
         } else {
-            return $this->db->get_where('absensi', ['id_absen' => $id])->result_array();
+            $this->db->select(
+                'absensi.id_absen,
+                pegawai.nama_depan,
+                pegawai.nama_belakang,
+                divisi.nama_divisi,
+                jabatan.nama_jabatan,
+                concat(absensi.periode_bulan, "-", absensi.periode_tahun) as periode,
+                absensi.tanggal as tanggal_absen,
+                absensi.status,
+                absensi.keterangan'
+            );
+            $this->db->from('absensi');
+            $this->db->join('pegawai', 'absensi.id_pegawai = pegawai.id_pegawai');
+            $this->db->join('jabatan', 'pegawai.id_jabatan = jabatan.id_jabatan');
+            $this->db->join('divisi', 'pegawai.id_divisi = divisi.id_divisi');
+            $this->db->where('absensi.id_absen', $id);
+            $query = $this->db->get();
+            return $query->result_array();
         }
     }
 
